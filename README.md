06912e50d201aca64ee2b7bbd820fa70516c06a0
sourceCoder84 <zhizunbaozaixian@163.com>
2019-03-12 15:04:03 +0800

    turn on leds by key

--- README.md
+++ README.md
@@ -1,13 +1,17 @@
 # luoban
 the program about 2440 
 
-005_len_02
+006_len
 编写C语言函数设置LED
 1. 关闭看门狗
 2. 自动识别nor启动还是nand启动
 3. 循环点亮led
 4. 添加寄存器头文件: s3c2440_soc.h
-
-注意 将start.S 中的mov r1 #0       换成 ldr r1 =0
-				moveq sp, #4096 换成 ldreq sp, #4096 	
-  转成汇编代码是一样的
+5. 使用按键操作led
+	/*按键点亮
+	 * 对应关系:
+	 * 按键                       led
+	 * ENIT0(GPF0)                D12(GPF6)
+	 * ENIT2(GPF2)                D11(GPF5)
+	 * ENIT11(GPG3)               D10(GPF4)
+	 */	
--- led.c
+++ led.c
@@ -8,21 +8,59 @@ void delay(volatile int d){
 
 int main(void)
 {
-		int vals[3] = {3, 5, 6 };
+		int val1, val2;
 
-		/*设置GPFCON让GPF4/5/6配置为输出引脚*/
+		/*设置GPFCON让GPF4(D10)/5(D11)/6(D12)配置为输出引脚*/
 		GPFCON &= ~((3 << 8) | (3 << 10) | (3 << 12));
 		GPFCON |= ((1 << 8) | (1 << 10) | (1 << 12));
 
-		/*循环点亮*/
-		int  i = 0;
+		/*设置按键控制寄存器为输入引脚*/
+		/*GPF0(EINT0), GPF2(EINT2)  GPG3(EINT11)*/
+		GPFCON &= ~((3 << 0) | (3 << 4));
+		GPGCON &= ~(3 << 6);
+
+		
+		
+		/*按键点亮
+		 * 对应关系:
+		 * 按键                               led
+		 * ENIT0(GPF0)                D12(GPF6)
+		 * ENIT2(GPF2)                D11(GPF5)
+		 * ENIT11(GPG3)               D10(GPF4)
+		 */
 		while(1){
 
-			for(i = 0; i < 3; i++){
-				GPFDAT &= ~(7 << 4);
-				GPFDAT |= (vals[i] << 4);
-				delay(100000);
+			val1 = GPFDAT;
+			val2 = GPGDAT;
+
+			if(val1 & (1 << 0)){                               
+				/*如果是高电平表明是松开状态，led熄灭
+				 * GPFDAT6 = 1	
+				 */
+				 GPFDAT |= (1 << 6);
+			}else{
+				/*GPFDAT6 = 0*/
+				 GPFDAT &= ~(1 << 6);
 			}
+			if(val1 & (1 << 2)){                               
+				/*如果是高电平表明是松开状态，led熄灭 
+				 * GPFDAT5 = 1	
+				 */	
+				 GPFDAT |= (1 << 5);
+			}else{
+				/*GPFDAT5 = 0*/
+				 GPFDAT &= ~(1 << 5);
+			}
+			if(val2 & (1 << 3)){                               
+				/*如果是高电平表明是松开状态，led熄灭
+				 * GPFDAT4 = 1	
+				 */	
+				  GPFDAT |= (1 << 4);
+			}else{
+				/* GPFDAT4 = 0 */
+				 GPFDAT &= ~(1 << 4);
+			}	
+			
 		}
 	return 0;
 }
