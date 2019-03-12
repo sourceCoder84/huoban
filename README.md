c2f2d51f9a8b75e0e597775b99163e3d342020b9
sourceCoder84 <zhizunbaozaixian@163.com>
2019-03-12 13:53:04 +0800

    add head file : s3c2440_soc.h

--- README.md
+++ README.md
@@ -1,11 +1,12 @@
 # luoban
 the program about 2440 
 
-004_len_on
+005_len_02
 编写C语言函数设置LED
 1. 关闭看门狗
 2. 自动识别nor启动还是nand启动
 3. 循环点亮led
+4. 添加寄存器头文件: s3c2440_soc.h
 
 注意 将start.S 中的mov r1 #0       换成 ldr r1 =0
 				moveq sp, #4096 换成 ldreq sp, #4096 	
--- led.c
+++ led.c
@@ -1,3 +1,4 @@
+#include "s3c2440_soc.h"
 
 void delay(volatile int d){
 
@@ -8,20 +9,18 @@ void delay(volatile int d){
 int main(void)
 {
 		int vals[3] = {3, 5, 6 };
-		volatile unsigned int *pGPFCON = (volatile unsigned int *)0x56000050;
-		volatile unsigned int *pGPFDAT = (volatile unsigned int *)0x56000054;
 
 		/*设置GPFCON让GPF4/5/6配置为输出引脚*/
-		*pGPFCON &= ~((3 << 8) | (3 << 10) | (3 << 12));
-		*pGPFCON |= ((1 << 8) | (1 << 10) | (1 << 12));
+		GPFCON &= ~((3 << 8) | (3 << 10) | (3 << 12));
+		GPFCON |= ((1 << 8) | (1 << 10) | (1 << 12));
 
 		/*循环点亮*/
 		int  i = 0;
 		while(1){
 
 			for(i = 0; i < 3; i++){
-				*pGPFDAT &= ~(7 << 4);
-				*pGPFDAT |= (vals[i] << 4);
+				GPFDAT &= ~(7 << 4);
+				GPFDAT |= (vals[i] << 4);
 				delay(100000);
 			}
 		}
